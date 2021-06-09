2b249f257fec144be35abee28da92a6a01b96493
Rohan Jo <rohan54518@gmail.com>
2021-06-10 02:36:58 +0530

    updated app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express=require('express')
 const {inbound}=require('./api/routes/inbound')
 const outbound=require('./api/routes/outbound')
 const app=express()
+const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
@@ -32,7 +33,7 @@ mongoose.connection.on("connected",()=>{
 mongoose.connection.on('error',(err)=>{
     console.error("Error connecting to Mongo",err)
 })
-app.listen(3000,()=>{
+app.listen(process.env.PORT || port,(req,res)=>{
     console.log("listening:3000")
 })
 
